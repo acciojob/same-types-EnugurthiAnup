7bd197807dcbd45131d864084e0107ac44d5a78a
EnugurthiAnup <eanup.736@gmail.com>
2025-07-29 01:06:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function isSameType(value1, value2) {
-  // Convert prompt inputs to values (prompt always returns strings)
-  const val1 = value1 === "" ? value1 : isNaN(value1) ? value1 : Number(value1);
-  const val2 = value2 === "" ? value2 : isNaN(value2) ? value2 : Number(value2);
+function isSameType(value1, value2) {    
+  // Handle NaN check first
+  if (Number.isNaN(Number(value1)) && Number.isNaN(Number(value2))) return true;
 
-  // Check for NaN
-  if (Number.isNaN(val1) && Number.isNaN(val2)) return true;
+  // If one is NaN but not both
+  if (Number.isNaN(Number(value1)) || Number.isNaN(Number(value2))) return false;
 
-  // Return true if both are of the same type
-  return typeof val1 === typeof val2;
+  // Compare types directly (string inputs stay string, numbers get converted)
+  return typeof value1 === typeof value2;
 }
 
-// Do not change the code below
+// Do not change below
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
+
 alert(isSameType(value1, value2));
